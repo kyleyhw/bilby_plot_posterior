24086e517886b728fa5965b7b3c1cda875765858
kyleyhw <kyleyhw@gmail.com>
2023-08-14 19:35:13 -0400

    common radius data loader written, plots generate but don't cut off properly

--- compare_binary_love.py
+++ compare_binary_love.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
-from data_loaders import DataLoaderJSON, DataLoaderH5
+import common_radius_conversion_functions
+from data_loaders import DataLoaderJSON, DataLoaderH5, DataLoaderCommonRadius
 from plot_posterior import PlotPosterior
 
 parameters = ['lambda_1', 'lambda_2']
@@ -18,6 +19,20 @@ binary_love_plotter = PlotPosterior(binary_love_data.posteriors_dict[parameters[
                                   binary_love_data.injection_dict[parameters[0]], binary_love_data.injection_dict[parameters[1]],
                                     limit_at_axes=False, limit_at_diagonal=True)
 
+common_radius_filename = 'bns_common_radius_result'
+common_radius_data = DataLoaderCommonRadius(filename='data/' + common_radius_filename + '.json', parameters=['lambda_2', 'mass_ratio'])
+common_radius_CR_samples = common_radius_data.posteriors['common_radius_uniform']
+common_radius_lambda_1 = common_radius_conversion_functions.common_radius_lambda_2_to_lambda_1_lambda_2_manual_marginalisation(
+    common_radius_CR_samples, common_radius_data.posteriors_dict['lambda_2'],
+    common_radius_data.posteriors_dict['mass_ratio'])[0]
+common_radius_data.posteriors_dict['lambda_1'] = common_radius_lambda_1
+common_radius_data.injection_dict['lambda_1'] = 545
+
+print(common_radius_data.posteriors_dict['lambda_1'], common_radius_data.posteriors_dict['lambda_2'])
+
+common_radius_plotter = PlotPosterior(common_radius_data.posteriors_dict[parameters[0]], common_radius_data.posteriors_dict[parameters[1]],
+                                    common_radius_data.injection_dict['lambda_1'], common_radius_data.injection_dict['lambda_2'],
+                                    limit_at_axes=False, limit_at_diagonal=False)
 
 
 fig = plt.figure(figsize=(8, 8))
@@ -27,14 +42,20 @@ ax.set(box_aspect=1)
 ax_histx = ax.inset_axes([0, 1.05, 1, 0.25], sharex=ax)
 ax_histy = ax.inset_axes([1.05, 0, 0.25, 1], sharey=ax)
 
-zero_spin_plotter.scatter_hist(x=zero_spin_plotter.data1, y=zero_spin_plotter.data2, ax=ax,
-                               injected_x=zero_spin_plotter.injected_1, injected_y=zero_spin_plotter.injected_2,
-                               ax_histx=ax_histx, ax_histy=ax_histy, scatter_color='blue', color='orange', label='w/o binary Love')
+# zero_spin_plotter.scatter_hist(x=zero_spin_plotter.data1, y=zero_spin_plotter.data2, ax=ax,
+#                                injected_x=zero_spin_plotter.injected_1, injected_y=zero_spin_plotter.injected_2,
+#                                ax_histx=ax_histx, ax_histy=ax_histy, scatter_color='blue', color='orange', label='w/o binary Love')
+#
+# binary_love_plotter.scatter_hist(x=binary_love_plotter.data1, y=binary_love_plotter.data2, ax=ax,
+#                                injected_x=binary_love_plotter.injected_1, injected_y=binary_love_plotter.injected_2,
+#                                ax_histx=ax_histx, ax_histy=ax_histy, scatter_color='red', color='green', label='with binary Love')
 
-binary_love_plotter.scatter_hist(x=binary_love_plotter.data1, y=binary_love_plotter.data2, ax=ax,
-                               injected_x=binary_love_plotter.injected_1, injected_y=binary_love_plotter.injected_2,
-                               ax_histx=ax_histx, ax_histy=ax_histy, scatter_color='red', color='green', label='with binary Love')
+common_radius_plotter.scatter_hist(x=common_radius_plotter.data1, y=common_radius_plotter.data2, ax=ax,
+                               injected_x=common_radius_plotter.injected_1, injected_y=common_radius_plotter.injected_2,
+                               ax_histx=ax_histx, ax_histy=ax_histy, scatter_color='black', color='black', label='with common radius')
 
 plt.axis('scaled')
+plt.xlabel('$\Lambda_1$')
+plt.ylabel('$\Lambda_2$')
 plt.savefig('plots/binary_love_comparison_plot.png')
 # plt.show()
--- data_loaders.py
+++ data_loaders.py
@@ -29,3 +29,22 @@ class DataLoaderH5:
         for parameter in parameters:
             self.posteriors_dict[parameter] = posteriors[parameter][:]
             self.injection_dict[parameter] = injection_parameters[parameter][()]
+
+
+
+import numpy as np
+
+class DataLoaderCommonRadius:
+    def __init__(self, filename, parameters):
+        with open(filename) as file:
+            data = json.load(file)
+
+        self.posteriors = data['nested_samples']['content']
+        injection_parameters = data['injection_parameters']
+
+        self.posteriors_dict = {}
+        self.injection_dict = {}
+
+        for parameter in parameters:
+            self.posteriors_dict[parameter] = np.array(self.posteriors[parameter])
+            self.injection_dict[parameter] = injection_parameters[parameter]
